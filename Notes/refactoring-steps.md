6607401a0000d9e2c971b26308a10198f99a7be0
Priti Biyani <pritib@priti.local>
2018-09-06 12:55:54 +0530

    - Used extract method for statement method. - Renamed variables in extracted method body - Got rid of amount in statatement

---
+++ Notes/refactoring-steps.md
@@ -0,0 +1,20 @@
+### Starting with feature 
+1. When you have to add a feature to a program but the code is not structured in a convenient way, first refactor the program to make it easy to add the feature, then add the feature.
+2. Before you start refactoring, make sure you have a solid suite of tests. These tests must be self-checking.
+
+### Steps for refactoring: 
+
+1. Add test for existing code if not present already 
+2. Statement in Customer looks a crux of application and doing lots of things. Try to refactor that first by understanding what it is doing. 
+
+
+##### Decomposing and restructuring Statement Method
+
+- Use extract method for statement. The code chunk we can target in statement calculation is obviously Switch statement. 
+- When we by default use refactor, it will refactor with amount as well. And then we realize that total amount is not concern of extracted method, and hence get rid of `totalAmount` from extracted method and change method signature. 
+- Run the test and everything should be passing. 
+- ```Refactoring changes the programs in small steps, so if you make a mistake, it is easy to find where the bug is.```
+
+
+
+
--- src/main/java/com/movie_rental/Customer.java
+++ src/main/java/com/movie_rental/Customer.java
@@ -27,36 +27,21 @@ class Customer {
         Enumeration rentals = _rentals.elements();
         String result = "Rental Record for " + getName() + "\n";
         while (rentals.hasMoreElements()) {
-            double thisAmount = 0;
-            Rental each = (Rental) rentals.nextElement();
+            Rental rental = (Rental) rentals.nextElement();
 
             //determine amounts for each line
-            switch (each.getMovie().getPriceCode()) {
-                case Movie.REGULAR:
-                    thisAmount += 2;
-                    if (each.getDaysRented() > 2)
-                        thisAmount += (each.getDaysRented() - 2) * 1.5;
-                    break;
-                case Movie.NEW_RELEASE:
-                    thisAmount += each.getDaysRented() * 3;
-                    break;
-                case Movie.CHILDRENS:
-                    thisAmount += 1.5;
-                    if (each.getDaysRented() > 3)
-                        thisAmount += (each.getDaysRented() - 3) * 1.5;
-                    break;
-            }
+            double amount = getAmountFor(rental);
 
             // add frequent renter points
             frequentRenterPoints++;
             // add bonus for a two day new release rental
-            if ((each.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
-                    each.getDaysRented() > 1) frequentRenterPoints++;
+            if ((rental.getMovie().getPriceCode() == Movie.NEW_RELEASE) &&
+                    rental.getDaysRented() > 1) frequentRenterPoints++;
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle() + "\t" +
-                    String.valueOf(thisAmount) + "\n";
-            totalAmount += thisAmount;
+            result += "\t" + rental.getMovie().getTitle() + "\t" +
+                    String.valueOf(amount) + "\n";
+            totalAmount += amount;
 
         }
         //add footer lines
@@ -64,5 +49,25 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) +
                 " frequent renter points";
         return result;
+    }
+
+    private double getAmountFor(Rental rental) {
+        double amount = 0;
+        switch (rental.getMovie().getPriceCode()) {
+            case Movie.REGULAR:
+                amount  = 2;
+                if (rental.getDaysRented() > 2)
+                    amount += (rental.getDaysRented() - 2) * 1.5;
+                break;
+            case Movie.NEW_RELEASE:
+                amount = rental.getDaysRented() * 3;
+                break;
+            case Movie.CHILDRENS:
+                amount = 1.5;
+                if (rental.getDaysRented() > 3)
+                    amount += (rental.getDaysRented() - 3) * 1.5;
+                break;
+        }
+        return amount;
     }
 }
